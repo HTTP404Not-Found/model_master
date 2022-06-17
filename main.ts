58fd1c51a4e0aa01ae2e0f56a9da8baa6eb8062f
HTTP404 <46433848+HTTP404Not-Found@users.noreply.github.com>
2022-06-17 10:24:59 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ OLED.init(128, 64)
 basic.forever(function () {
     House.readDHT11(DigitalPin.P0)
     WiFiIoT.sendThingspeak(
-    "",
+    "72U5E5U29FZF5V2E",
     SmartCity.read_light_sensor(AnalogPin.P1),
     House.readTemperatureData(House.Temp_degree.degree_Celsius),
     House.readHumidityData()
