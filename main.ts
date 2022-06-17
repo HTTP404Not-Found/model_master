200bf82c48b8a96aed75cdabcc092c36e6cdab95
HTTP404 <46433848+HTTP404Not-Found@users.noreply.github.com>
2022-06-18 06:45:42 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,20 @@
 WiFiIoT.on_wifi_connect(function (IP_Address, Device_ID) {
     OLED.writeStringNewLine("WIFI ok")
 })
+radio.onReceivedString(function (receivedString) {
+    House.Turn180Servo(180, AnalogPin.P0)
+    pins.servoWritePin(AnalogPin.P0, 180)
+})
 WiFiIoT.initializeWifi(SerialPin.P16, SerialPin.P8)
 WiFiIoT.setWifi("SPSS_IOT", "iot@spss")
 OLED.init(128, 64)
+radio.setGroup(1)
 basic.forever(function () {
     House.readDHT11(DigitalPin.P0)
     WiFiIoT.sendThingspeak(
     "72U5E5U29FZF5V2E",
     SmartCity.read_light_sensor(AnalogPin.P1),
-    House.readTemperatureData(House.Temp_degree.degree_Celsius),
-    House.readHumidityData()
+    House.readTemperatureData(House.Temp_degree.degree_Celsius)
     )
     basic.pause(1000)
 })
