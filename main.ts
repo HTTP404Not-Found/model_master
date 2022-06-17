6a318f539de6f0a1e5beff49b109077d146191db
HTTP404 <46433848+HTTP404Not-Found@users.noreply.github.com>
2022-06-17 10:18:49 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+WiFiIoT.on_wifi_connect(function (IP_Address, Device_ID) {
+    OLED.writeStringNewLine("WIFI ok")
+})
+WiFiIoT.initializeWifi(SerialPin.P16, SerialPin.P8)
+WiFiIoT.setWifi("", "")
+OLED.init(128, 64)
+basic.forever(function () {
+    House.readDHT11(DigitalPin.P0)
+    WiFiIoT.sendThingspeak(
+    "",
+    SmartCity.read_light_sensor(AnalogPin.P1),
+    House.readTemperatureData(House.Temp_degree.degree_Celsius),
+    House.readHumidityData()
+    )
+    basic.pause(1000)
+})
