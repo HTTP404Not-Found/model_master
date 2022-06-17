d47205b827bd17d9fc2ea70c677f137aa58e6271
HTTP404 <46433848+HTTP404Not-Found@users.noreply.github.com>
2022-06-18 06:56:11 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,11 +4,22 @@ WiFiIoT.on_wifi_connect(function (IP_Address, Device_ID) {
 radio.onReceivedString(function (receivedString) {
     House.Turn180Servo(180, AnalogPin.P0)
     pins.servoWritePin(AnalogPin.P0, 180)
+    strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    basic.pause(1000)
+    strip.showColor(neopixel.colors(NeoPixelColors.Orange))
+    basic.pause(1000)
+    strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
+    basic.pause(1000)
+    strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    basic.pause(1000)
 })
+let strip: neopixel.Strip = null
 WiFiIoT.initializeWifi(SerialPin.P16, SerialPin.P8)
 WiFiIoT.setWifi("SPSS_IOT", "iot@spss")
 OLED.init(128, 64)
 radio.setGroup(1)
+strip = neopixel.create(DigitalPin.P0, 1, NeoPixelMode.RGB)
+let range = strip.range(0, 1)
 basic.forever(function () {
     House.readDHT11(DigitalPin.P0)
     WiFiIoT.sendThingspeak(
