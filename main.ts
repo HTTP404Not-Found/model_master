555afd7b6c6933e131f2176ed880709791f5a523
HTTP404 <46433848+HTTP404Not-Found@users.noreply.github.com>
2022-06-18 12:24:37 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,36 +4,32 @@ WiFiIoT.on_wifi_connect(function (IP_Address, Device_ID) {
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "ok") {
         pins.digitalWritePin(DigitalPin.P2, 1)
-        pins.servoWritePin(AnalogPin.P10, 180)
         pins.servoWritePin(AnalogPin.P4, 180)
-        basic.pause(1500)
-        pins.servoWritePin(AnalogPin.P4, 90)
-        strip.showColor(neopixel.colors(NeoPixelColors.Red))
-        basic.pause(1000)
+        pins.servoWritePin(AnalogPin.P3, 180)
+        basic.pause(2050)
+        pins.servoWritePin(AnalogPin.P3, 90)
+        SmartCity.turn_white_led(1023, AnalogPin.P14)
+        pins.digitalWritePin(DigitalPin.P10, 1)
     }
     if (receivedString == "bye") {
         pins.digitalWritePin(DigitalPin.P2, 0)
-        pins.servoWritePin(AnalogPin.P10, 0)
         pins.servoWritePin(AnalogPin.P4, 0)
-        basic.pause(1500)
-        pins.servoWritePin(AnalogPin.P4, 90)
-        strip.clear()
+        pins.servoWritePin(AnalogPin.P3, 0)
+        basic.pause(2100)
+        pins.servoWritePin(AnalogPin.P3, 90)
+        pins.digitalWritePin(DigitalPin.P10, 0)
+        SmartCity.turn_white_led(0, AnalogPin.P14)
     }
 })
-let strip: neopixel.Strip = null
 WiFiIoT.initializeWifi(SerialPin.P16, SerialPin.P8)
 WiFiIoT.setWifi("SPSS_IOT", "iot@spss")
 OLED.init(128, 64)
 radio.setGroup(1)
-strip = neopixel.create(DigitalPin.P0, 1, NeoPixelMode.RGB)
-let range = strip.range(0, 1)
-pins.servoWritePin(AnalogPin.P10, 0)
 basic.forever(function () {
-    House.readDHT11(DigitalPin.P0)
     WiFiIoT.sendThingspeak(
     "72U5E5U29FZF5V2E",
     SmartCity.read_light_sensor(AnalogPin.P1),
-    House.readTemperatureData(House.Temp_degree.degree_Celsius)
+    SmartCity.readData(SmartCity.DHT11dataType.temperature, DigitalPin.P0)
     )
     basic.pause(1000)
 })
