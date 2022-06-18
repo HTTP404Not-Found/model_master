019e915cb85e04ed44da50707b7cf8a60ecac23a
HTTP404 <46433848+HTTP404Not-Found@users.noreply.github.com>
2022-06-18 10:54:52 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,16 +2,23 @@ WiFiIoT.on_wifi_connect(function (IP_Address, Device_ID) {
     OLED.writeStringNewLine("WIFI ok")
 })
 radio.onReceivedString(function (receivedString) {
-    House.Turn180Servo(180, AnalogPin.P0)
-    pins.servoWritePin(AnalogPin.P0, 180)
-    strip.showColor(neopixel.colors(NeoPixelColors.Red))
-    basic.pause(1000)
-    strip.showColor(neopixel.colors(NeoPixelColors.Orange))
-    basic.pause(1000)
-    strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
-    basic.pause(1000)
-    strip.showColor(neopixel.colors(NeoPixelColors.Green))
-    basic.pause(1000)
+    if (receivedString == "ok") {
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        pins.servoWritePin(AnalogPin.P10, 180)
+        pins.servoWritePin(AnalogPin.P4, 180)
+        basic.pause(1500)
+        pins.servoWritePin(AnalogPin.P4, 90)
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+        basic.pause(1000)
+    }
+    if (receivedString == "bye") {
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.servoWritePin(AnalogPin.P10, 0)
+        pins.servoWritePin(AnalogPin.P4, 0)
+        basic.pause(1500)
+        pins.servoWritePin(AnalogPin.P4, 90)
+        strip.clear()
+    }
 })
 let strip: neopixel.Strip = null
 WiFiIoT.initializeWifi(SerialPin.P16, SerialPin.P8)
@@ -20,6 +27,7 @@ OLED.init(128, 64)
 radio.setGroup(1)
 strip = neopixel.create(DigitalPin.P0, 1, NeoPixelMode.RGB)
 let range = strip.range(0, 1)
+pins.servoWritePin(AnalogPin.P10, 0)
 basic.forever(function () {
     House.readDHT11(DigitalPin.P0)
     WiFiIoT.sendThingspeak(
